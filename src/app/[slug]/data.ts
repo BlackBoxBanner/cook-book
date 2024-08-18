242b287dd9aa836151b462268129cd11ad0d4d78
Sueksit Vachirakumthorn <81872375+BlackBoxBanner@users.noreply.github.com>
2024-08-18 13:55:35 +0700

    Update recipe data and styling in [slug]/data.ts

--- src/app/[slug]/data.ts
+++ src/app/[slug]/data.ts
@@ -1,85 +1,141 @@
-import { Ingredient } from "@/components/basic/recipe";
+import {Ingredient} from "@/components/basic/recipe";
 
 type CookBook = {
-	name: string;
-	slug: string;
-	date: string;
-	description: string;
-	ingredients: Ingredient[];
-	steps: FirstStep[];
+  name: string;
+  slug: string;
+  date: string;
+  description: string;
+  ingredients: Ingredient[];
+  steps: FirstStep[];
 };
 
 type FirstStep = {
-	content: string;
-	step?: string[];
+  content: string;
+  step?: string[];
 };
 
 export const cookbook: CookBook[] = [
-	{
-		name: "เลมอนเคิร์ด",
-		slug: "lemon-curd",
-		date: new Date("2024-06-03").toDateString(),
-		description:
-			"เลมอนเคิร์ด เป็นซอสลักษณะคล้ายคัสตาร์ด มีรสชาติออกเปรี้ยวหวานมีกลิ่นหอมอ่อนๆของเลมอนเป็นหลัก นิยมนำไปใช้ทาขนมปัง, แครกเกอร์ หรือเอาไปเป็นส่วนประกอบของขนมอื่นๆ เช่น คุกกี้ พายหรือทาร์ต",
-		ingredients: [
-			{ ingredient: "เนยเค็ม", quantity: "70", units: "g" },
-			{ ingredient: "เกลือ", quantity: "0.125", units: "tsp" },
-			{ ingredient: "น้ำตาลทราย", quantity: "110", units: "g" },
-			{ ingredient: "น้ำเลม่อน", quantity: "100", units: "ml" },
-			{ ingredient: "ผิวเลม่อน", quantity: "2", units: "tbsp" },
-			{ ingredient: "ไข่แดง", quantity: "3", units: "unit" },
-			{ ingredient: "ไข่ขาว", quantity: "1", units: "unit" },
-		],
-		steps: [
-			{
-				content: "เตรียมวัตถุดิบ",
-				step: [
-					"นำผิวเลมอนขูดมาคลุกกับน้ำตาลทรายในอ่างผสมให้เข้ากันดี",
-					"ทิ้งไว้ประมาณ 15 นาทีเพื่อให้น้ำมันหอมระเหยจากผิวเลมอนซึมเข้ากับน้ำตาล",
-				],
-			},
-			{
-				content: "ผสมส่วนผสม",
-				step: [
-					"แยกไข่แดง 3 ฟอง และไข่ขาว 1 ฟองลงในอ่างผสมเดียวกัน (ตามสูตร)",
-					"ใส่ผิวเลมอนและน้ำตาลที่คลุกไว้ลงในอ่างผสมที่มีไข่",
-					"เติมน้ำเลมอนลงไป แล้วคนให้เข้ากัน",
-				],
-			},
-			{
-				content: "ให้ความร้อน",
-				step: [
-					"ยกอ่างผสมวางบนหม้อน้ำเดือดอ่อนๆ (ทำการตุ๋น)",
-					"ใช้ตะกร้อคนส่วนผสมอย่างต่อเนื่อง เพื่อป้องกันการเกิดเป็นก้อน",
-					"คนจนส่วนผสมเริ่มข้นขึ้น (ใช้เวลาประมาณ 10-15 นาที)",
-					"ใส่เนยจืดหั่นเป็นชิ้นเล็กๆ ลงไปในส่วนผสม คนจนเนยละลายและเข้ากันดีกับส่วนผสม",
-				],
-			},
-		],
-	},
-	{
-		name: "ซอสมะพร้าว",
-		slug: "coconut-sauce",
-		date: new Date("2024-06-03").toDateString(),
-		description: "ลาวามะพร้าวอ่อน",
-		ingredients: [
-			{ ingredient: "น้ำมะพร้าว", quantity: "200", units: "g" },
-			{ ingredient: "กะทิ", quantity: "100", units: "g" },
-			{ ingredient: "น้ำตาล", quantity: "30", units: "g" },
-			{ ingredient: "แป้งกวนใส้", quantity: "10", units: "g" },
-			{ ingredient: "เนยเค็ม", quantity: "10", units: "g" },
-			{ ingredient: "เนื้อมะพร้าว", quantity: "300", units: "g" },
-		],
-		steps: [
-			{
-				content:
-					"นำน้ำมะพร้าว กะทิ และน้ำตาลทราย ใส่ลงในหม้อ คนให้เข้ากัน ขั้นตอนนี้ชิมรสได้ตามชอบ จะตัดรสด้วยเกลือนิดหน่อยก็ได้",
-			},
-			{
-				content:
-					"ใส่แป้งข้าวโพด คนให้แป้งละลาย นำขึ้นตั้งไฟปานกลางค่อนข้างอ่อน ระหว่างตั้งไฟให้คนตลอดค่ะ เดี๋ยวจะไหม้ก้นหม้อ คนจนซอสข้น หลังจากนั้นใส่เนื้อมะพร้าวอ่อน",
-			},
-			{ content: "คนให้เข้ากัน รอเดือดนิดหน่อยแล้วปิดไฟ พักให้เย็น" },
-		],
-	},
+  {
+    name: "เลมอนเคิร์ด",
+    slug: "lemon-curd",
+    date: new Date("2024-06-03").toDateString(),
+    description:
+      "เลมอนเคิร์ด เป็นซอสลักษณะคล้ายคัสตาร์ด มีรสชาติออกเปรี้ยวหวานมีกลิ่นหอมอ่อนๆของเลมอนเป็นหลัก นิยมนำไปใช้ทาขนมปัง, แครกเกอร์ หรือเอาไปเป็นส่วนประกอบของขนมอื่นๆ เช่น คุกกี้ พายหรือทาร์ต",
+    ingredients: [
+      {ingredient: "เนยเค็ม", quantity: "70", units: "g"},
+      {ingredient: "เกลือ", quantity: "0.125", units: "tsp"},
+      {ingredient: "น้ำตาลทราย", quantity: "110", units: "g"},
+      {ingredient: "น้ำเลม่อน", quantity: "100", units: "ml"},
+      {ingredient: "ผิวเลม่อน", quantity: "2", units: "tbsp"},
+      {ingredient: "ไข่แดง", quantity: "3", units: "unit"},
+      {ingredient: "ไข่ขาว", quantity: "1", units: "unit"},
+    ],
+    steps: [
+      {
+        content: "เตรียมวัตถุดิบ",
+        step: [
+          "นำผิวเลมอนขูดมาคลุกกับน้ำตาลทรายในอ่างผสมให้เข้ากันดี",
+          "ทิ้งไว้ประมาณ 15 นาทีเพื่อให้น้ำมันหอมระเหยจากผิวเลมอนซึมเข้ากับน้ำตาล",
+        ],
+      },
+      {
+        content: "ผสมส่วนผสม",
+        step: [
+          "แยกไข่แดง 3 ฟอง และไข่ขาว 1 ฟองลงในอ่างผสมเดียวกัน (ตามสูตร)",
+          "ใส่ผิวเลมอนและน้ำตาลที่คลุกไว้ลงในอ่างผสมที่มีไข่",
+          "เติมน้ำเลมอนลงไป แล้วคนให้เข้ากัน",
+        ],
+      },
+      {
+        content: "ให้ความร้อน",
+        step: [
+          "ยกอ่างผสมวางบนหม้อน้ำเดือดอ่อนๆ (ทำการตุ๋น)",
+          "ใช้ตะกร้อคนส่วนผสมอย่างต่อเนื่อง เพื่อป้องกันการเกิดเป็นก้อน",
+          "คนจนส่วนผสมเริ่มข้นขึ้น (ใช้เวลาประมาณ 10-15 นาที)",
+          "ใส่เนยจืดหั่นเป็นชิ้นเล็กๆ ลงไปในส่วนผสม คนจนเนยละลายและเข้ากันดีกับส่วนผสม",
+        ],
+      },
+    ],
+  },
+  {
+    name: "ซอสมะพร้าว",
+    slug: "coconut-sauce",
+    date: new Date("2024-06-03").toDateString(),
+    description: "ลาวามะพร้าวอ่อน",
+    ingredients: [
+      {ingredient: "น้ำมะพร้าว", quantity: "200", units: "g"},
+      {ingredient: "กะทิ", quantity: "100", units: "g"},
+      {ingredient: "น้ำตาล", quantity: "30", units: "g"},
+      {ingredient: "แป้งกวนใส้", quantity: "10", units: "g"},
+      {ingredient: "เนยเค็ม", quantity: "10", units: "g"},
+      {ingredient: "เนื้อมะพร้าว", quantity: "300", units: "g"},
+    ],
+    steps: [
+      {
+        content:
+          "นำน้ำมะพร้าว กะทิ และน้ำตาลทราย ใส่ลงในหม้อ คนให้เข้ากัน ขั้นตอนนี้ชิมรสได้ตามชอบ จะตัดรสด้วยเกลือนิดหน่อยก็ได้",
+      },
+      {
+        content:
+          "ใส่แป้งข้าวโพด คนให้แป้งละลาย นำขึ้นตั้งไฟปานกลางค่อนข้างอ่อน ระหว่างตั้งไฟให้คนตลอดค่ะ เดี๋ยวจะไหม้ก้นหม้อ คนจนซอสข้น หลังจากนั้นใส่เนื้อมะพร้าวอ่อน",
+      },
+      {content: "คนให้เข้ากัน รอเดือดนิดหน่อยแล้วปิดไฟ พักให้เย็น"},
+    ],
+  },
+  {
+    name: "นมชมพู (นม)",
+    slug: "pink-milk-milk",
+    date: new Date("2024-08-18").toDateString(),
+    description:
+      "นมชมพูแบบดั้งเดิมที่ใช้ส่วนผสมนมเป็นหลัก ให้รสชาติหอมหวานและเข้มข้น",
+    ingredients: [
+      {ingredient: "น้ำเปล่า", quantity: "1000", units: "ml"},
+      {ingredient: "น้ำแดง เฮลบลูบอย", quantity: "200", units: "ml"},
+      {ingredient: "นมข้นจืด ส่วนที่ 1", quantity: "80", units: "g"},
+      {ingredient: "นมข้นจืด ส่วนที่ 2", quantity: "405", units: "g"},
+      {ingredient: "นมข้นหวาน", quantity: "115", units: "g"},
+    ],
+    steps: [
+      {content: "ต้มน้ำเปล่าให้เดือด"},
+      {
+        content: "เติมน้ำแดงขณะน้ำเดือด",
+        step: [
+          "ยังไม่ต้องปิดไฟ",
+          "ต้มต่อจนน้ำเดือดอีกรอบ",
+          "เมื่อเดือดแล้ว ปิดไฟ",
+        ],
+      },
+      {content: "ใส่นมข้นจืด ส่วนที่ 1", step: ["ผสมจนเข้ากันดี"]},
+      {content: "เติมนมข้นหวาน", step: ["ผสมจนเข้ากันดี"]},
+      {content: "ใส่นมข้นจืด ส่วนที่ 2", step: ["ผสมจนเข้ากันดี"]},
+    ],
+  },
+  {
+    name: "นมชมพู (ครีม)",
+    slug: "pink-milk-cream",
+    date: new Date("2024-08-18").toDateString(),
+    description:
+      "นมชมพูสูตรพิเศษที่ใช้ครีมเทียมเป็นส่วนผสม ให้ความหอมมันและเนื้อสัมผัสที่นุ่มนวล",
+    ingredients: [
+      {ingredient: "น้ำเปล่า", quantity: "1000", units: "ml"},
+      {ingredient: "น้ำแดง เฮลบลูบอย", quantity: "200", units: "ml"},
+      {ingredient: "ครีมเทียม", quantity: "60", units: "g"},
+      {ingredient: "นมข้นจืด", quantity: "405", units: "g"},
+      {ingredient: "นมข้นหวาน", quantity: "115", units: "g"},
+    ],
+    steps: [
+      {content: "ต้มน้ำเปล่าให้เดือด"},
+      {
+        content: "เติมน้ำแดงขณะน้ำเดือด",
+        step: [
+          "ยังไม่ต้องปิดไฟ",
+          "ต้มต่อจนน้ำเดือดอีกรอบ",
+          "เมื่อเดือดแล้ว ปิดไฟ",
+        ],
+      },
+      {content: "ใส่ครีมเทียม", step: ["ผสมจนเข้ากันดี"]},
+      {content: "เติมนมข้นหวาน", step: ["ผสมจนเข้ากันดี"]},
+      {content: "ใส่นมข้นจืด", step: ["ผสมจนเข้ากันดี"]},
+    ],
+  },
 ];
--- src/components/basic/recipe.tsx
+++ src/components/basic/recipe.tsx
@@ -1,194 +1,164 @@
 "use client";
 import {
-	Table,
-	TableBody,
-	TableCell,
-	TableHead,
-	TableHeader,
-	TableRow,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
 } from "@/components/ui/table";
-import { ChangeEvent, useEffect, useState } from "react";
-import { Button } from "@/components/ui/button";
-import { MinusIcon, PlusIcon } from "@radix-ui/react-icons";
-import { Checkbox } from "@/components/ui/checkbox";
-import { Label } from "../ui/label";
-
-type IngredientList =
-	| "เนยเค็ม"
-	| "เนยจืด"
-	| "เกลือ"
-	| "น้ำตาลทราย"
-	| "น้ำเลม่อน"
-	| "ผิวเลม่อน"
-	| "ไข่แดง"
-	| "ไข่ขาว"
-	| "แป้งแค้ก"
-	| "แป้ง"
-	| "นมจืด"
-	| "น้ำมะนาว"
-	| "เกลือป่น"
-	| "ผงฟู"
-	| "น้ำตาล"
-	| "ครีมออฟทาร์ทาร์"
-	| "น้ำมะพร้าว"
-	| "กะทิ"
-	| "แป้งกวนใส้"
-	| "เนื้อมะพร้าว"
-	| ""
-	| ""
-	| ""
-	| ""
-	| ""
-	| ""
-	| ""
-	| "";
+import {ChangeEvent, useEffect, useState} from "react";
+import {Button} from "@/components/ui/button";
+import {MinusIcon, PlusIcon} from "@radix-ui/react-icons";
+import {Checkbox} from "@/components/ui/checkbox";
+import {Label} from "../ui/label";
 
 export type Ingredient = {
-	ingredient: IngredientList;
-	quantity: string;
-	units:
-		| "tsp"
-		| "tbsp"
-		| "cup"
-		| "oz"
-		| "lb"
-		| "g"
-		| "kg"
-		| "ml"
-		| "l"
-		| "unit";
+  ingredient: String;
+  quantity: string;
+  units:
+    | "tsp"
+    | "tbsp"
+    | "cup"
+    | "oz"
+    | "lb"
+    | "g"
+    | "kg"
+    | "ml"
+    | "l"
+    | "unit";
 };
 
 function toFraction(value: number, on: boolean = false): string {
-	if (!on) return value.toString();
+  if (!on) return value.toString();
 
-	if (Number.isInteger(value)) {
-		return value.toString();
-	}
+  if (Number.isInteger(value)) {
+    return value.toString();
+  }
 
-	const gcd = (a: number, b: number): number => {
-		return b === 0 ? a : gcd(b, a % b);
-	};
+  const gcd = (a: number, b: number): number => {
+    return b === 0 ? a : gcd(b, a % b);
+  };
 
-	const precision = 1e-10; // Tolerance for floating-point comparison
-	let numerator = value;
-	let denominator = 1;
+  const precision = 1e-10; // Tolerance for floating-point comparison
+  let numerator = value;
+  let denominator = 1;
 
-	while (Math.abs(numerator - Math.round(numerator)) > precision) {
-		numerator *= 10;
-		denominator *= 10;
-	}
+  while (Math.abs(numerator - Math.round(numerator)) > precision) {
+    numerator *= 10;
+    denominator *= 10;
+  }
 
-	numerator = Math.round(numerator);
+  numerator = Math.round(numerator);
 
-	const commonDivisor = gcd(numerator, denominator);
-	numerator /= commonDivisor;
-	denominator /= commonDivisor;
+  const commonDivisor = gcd(numerator, denominator);
+  numerator /= commonDivisor;
+  denominator /= commonDivisor;
 
-	return `${numerator}/${denominator}`;
+  return `${numerator}/${denominator}`;
 }
 
-const RecipeTable = ({ ingredient }: { ingredient: Ingredient[] }) => {
-	const [list, setList] = useState<Ingredient[]>(ingredient);
-	const [portion, setPortion] = useState<string>("1");
-	const [decimal, setDecimal] = useState<boolean>(false);
+const RecipeTable = ({ingredient}: {ingredient: Ingredient[]}) => {
+  const [list, setList] = useState<Ingredient[]>(ingredient);
+  const [portion, setPortion] = useState<string>("1");
+  const [decimal, setDecimal] = useState<boolean>(false);
 
-	const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
-		const inputValue = event.target.value;
-		let portionValue = parseFloat(inputValue);
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const inputValue = event.target.value;
+    let portionValue = parseFloat(inputValue);
 
-		if (isNaN(portionValue) || portionValue < 1) {
-			portionValue = 1;
-		}
+    if (isNaN(portionValue) || portionValue < 1) {
+      portionValue = 1;
+    }
 
-		setPortion(String(portionValue));
-	};
+    setPortion(String(portionValue));
+  };
 
-	const reduceHandler = () => {
-		setPortion((e) => (parseFloat(e) > 1 ? String(parseFloat(e) - 1) : "1"));
-	};
+  const reduceHandler = () => {
+    setPortion((e) => (parseFloat(e) > 1 ? String(parseFloat(e) - 1) : "1"));
+  };
 
-	const increaseHandler = () => {
-		setPortion((e) => String(parseFloat(e) + 1));
-	};
+  const increaseHandler = () => {
+    setPortion((e) => String(parseFloat(e) + 1));
+  };
 
-	const valueChangeHandler = () => {
-		if (ingredient.length === 0) return;
-		if (portion === "") return;
-		setList(
-			ingredient.map((item) => {
-				return {
-					...item,
-					quantity: (
-						parseFloat(item.quantity) * parseFloat(portion)
-					).toString(),
-				};
-			})
-		);
-	};
+  const valueChangeHandler = () => {
+    if (ingredient.length === 0) return;
+    if (portion === "") return;
+    setList(
+      ingredient.map((item) => {
+        return {
+          ...item,
+          quantity: (
+            parseFloat(item.quantity) * parseFloat(portion)
+          ).toString(),
+        };
+      })
+    );
+  };
 
-	useEffect(() => {
-		valueChangeHandler();
-	}, [portion, ingredient]);
+  useEffect(() => {
+    valueChangeHandler();
+  }, [portion, ingredient]);
 
-	return (
-		<>
-			<div className="flex items-center justify-between gap-4">
-				<div className="flex justify-center items-center gap-4">
-					<Checkbox
-						id="decimal"
-						checked={decimal}
-						onCheckedChange={(e) => setDecimal(!!e)}
-					/>
-					<Label htmlFor="decimal">Decimal</Label>
-				</div>
-				<div className="flex gap-2 items-center justify-center">
-					<Button onClick={reduceHandler} variant={"ghost"}>
-						<MinusIcon />
-					</Button>
-					<span>
-						<input
-							value={portion}
-							type="text"
-							name="portion"
-							id="portion-input"
-							onChange={(e) => setPortion(e.target.value)}
-							onBlur={handleChange}
-							className="text-secondary-foreground outline-none w-[2rem] bg-transparent h-fit"
-						/>
-					</span>
-					<Button onClick={increaseHandler} variant={"ghost"}>
-						<PlusIcon />
-					</Button>
-				</div>
-			</div>
-			<Table>
-				<TableHeader>
-					<TableRow>
-						<TableHead className="w-[2rem]"></TableHead>
-						<TableHead>ingredient</TableHead>
-						<TableHead className="w-[6rem] md:w-[10rem]">quantity</TableHead>
-					</TableRow>
-				</TableHeader>
-				<TableBody>
-					{list.map((item, index) => {
-						return (
-							<TableRow key={index}>
-								<TableCell className="flex justify-center items-center">
-									<Checkbox />
-								</TableCell>
-								<TableCell>{item.ingredient}</TableCell>
-								<TableCell>{`${toFraction(
-									parseFloat(item.quantity),
-									!decimal
-								)} ${item.units}`}</TableCell>
-							</TableRow>
-						);
-					})}
-				</TableBody>
-			</Table>
-		</>
-	);
+  return (
+    <>
+      <div className="flex items-center justify-between gap-4">
+        <div className="flex justify-center items-center gap-4">
+          <Checkbox
+            id="decimal"
+            checked={decimal}
+            onCheckedChange={(e) => setDecimal(!!e)}
+          />
+          <Label htmlFor="decimal">Decimal</Label>
+        </div>
+        <div className="flex gap-2 items-center justify-center">
+          <Button onClick={reduceHandler} variant={"ghost"}>
+            <MinusIcon />
+          </Button>
+          <span>
+            <input
+              value={portion}
+              type="text"
+              name="portion"
+              id="portion-input"
+              onChange={(e) => setPortion(e.target.value)}
+              onBlur={handleChange}
+              className="text-secondary-foreground outline-none w-[2rem] bg-transparent h-fit"
+            />
+          </span>
+          <Button onClick={increaseHandler} variant={"ghost"}>
+            <PlusIcon />
+          </Button>
+        </div>
+      </div>
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-[2rem]"></TableHead>
+            <TableHead>ingredient</TableHead>
+            <TableHead className="w-[6rem] md:w-[10rem]">quantity</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {list.map((item, index) => {
+            return (
+              <TableRow key={index}>
+                <TableCell className="flex justify-center items-center">
+                  <Checkbox />
+                </TableCell>
+                <TableCell>{item.ingredient}</TableCell>
+                <TableCell>{`${toFraction(
+                  parseFloat(item.quantity),
+                  !decimal
+                )} ${item.units}`}</TableCell>
+              </TableRow>
+            );
+          })}
+        </TableBody>
+      </Table>
+    </>
+  );
 };
 
 export default RecipeTable;
